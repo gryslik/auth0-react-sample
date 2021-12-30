0ea7b325a5b3eb0a0919d879b801b148dc1cb9c8
Gregory Ryslik <gryslik@gmail.com>
2021-12-30 02:11:49 -0500

    Components now work. Need some clean up and can combine with mach4 app.

--- src/views/home.js
+++ src/views/home.js
@@ -2,15 +2,29 @@ import React, { Fragment } from "react";
 
 import { Hero, HomeContent } from "../components";
 import ProtectedRoute from "../auth/protected-route";
+import { withAuthenticationRequired } from '@auth0/auth0-react';
+import { useAuth0 } from "@auth0/auth0-react";
 
 
-const Home = () => (
+
+const Home = () => {
+  const { isAuthenticated, } = useAuth0();
+
+  return(
   <Fragment>
-    {/*<ProtectedRoute component={Hero} />*/}
-    <Hero />
-    <hr />
+    {/*<ProtectedRoute component={Hero} />*/},
+    {(isAuthenticated) ?  <Hero />: <div> You are currently logged out or user is not authorized for this system. Please login with an authorized user. </div>}
+    {/*<Hero />
+    <hr />*/}
     <HomeContent />
   </Fragment>
-);
+  );
+};
+
+/*
+export default withAuthenticationRequired(Home, {
+  // Show a message while the user waits to be redirected to the login page.
+  onRedirecting: () => (<div>Redirecting you to the login page...</div>)
+});*/
 
 export default Home;
